3142f30b9bb9a6818d8440108a1465c404a52945
Melodie Bernard <mbernard@z1r10p3.42lyon.fr>
2023-11-24 18:04:28 +0100

    commit

---
+++ ft_printf.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbernard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/24 14:18:48 by mbernard          #+#    #+#             */
+/*   Updated: 2023/11/24 17:56:02 by mbernard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+cspdiuxX%
+• %c Prints a single character.
+• %s Prints a string (as defined by the common C convention).
+• %p The void * pointer argument has to be printed in hexadecimal format.
+• %d Prints a decimal (base 10) number.
+• %i Prints an integer in base 10.
+• %u Prints an unsigned decimal (base 10) number.
+• %x Prints a number in hexadecimal (base 16) lowercase format.
+• %X Prints a number in hexadecimal (base 16) uppercase format.
+• %% Prints a percent sign.
+*/
+#include "ft_printf.h"
+
+int	ft_printf(const char *, ...)
+{
+}
